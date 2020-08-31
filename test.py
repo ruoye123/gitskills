00a7b7c41338c1a66daa2c35c9ea815ceaab1b42
ruoye123 <lhy19870421@163.com>
2020-08-31 14:28:04 +0800

    1st commit

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@author: Rita
+@software: XXX
+@file: test.py
+@time: 2020/8/31 14:26
+@desc:
+
+"""
+
+import random
+
+# print(random.randrange(0, 100, 2))
+list1 = random.shuffle([1, 2, 3, 4, 5, 6])
+
+print(list1)
